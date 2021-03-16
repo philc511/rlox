3acd49fd659d2196fdaa6b023d0b7c86abb85420
philc511 <phil.capon@fastmail.co.uk>
2021-03-16 21:01:38 +0000

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    let mut guess = String::new();
+
+    std::io::stdin()
+        .read_line(&mut guess);
+
+        println!("you guessed: {}", guess);
 }
